98d25c32a04284eaa041f00cca24469e4416ec32
nc-ellie-judge <ellie.judge@northcoders.com>
2023-11-30 16:13:33 +0000

    refactor: delete unused lines, tidy up

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,5 @@ export const app = express();
 app.use(express.json())
 
 app.get('/', (req, res) => res.send("<h1>Welcome to the drinks API!</h1>"));
-// app.get('/coffeelover', (req, res) => res.send('I like coffee!'));
-
-// app.use('/coffeelover', router);
 
 app.use('/', router);

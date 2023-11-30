5f3d956b163c0e5fa5c48b7b2486444895bbab99
nc-ellie-judge <ellie.judge@northcoders.com>
2023-11-30 12:30:08 +0000

    refactor: create routes file, add services file to handle business logic

---
+++ src/services/coffee_service.ts
@@ -0,0 +1,9 @@
+// Recall that the job of a service is to encapsulate the business logic, and to ask the model layer for any data required to do that. In our case, we have no persistent data storage, and therefore no model layer. So the coffee service has a simple job: encode any logic to do with coffee.
+// Any other logic to do with ‘coffee’ in this application as it grows would also be encapsulated in this service.
+
+export const getCoffee = (name = 'Latte') => {
+    return {
+        drinkType: 'Coffee',
+        name,
+    };
+};
